bbcfa2652d4633ededec38358516c6bf36d34c04
Mikhail Shcheglov <mr1302@ya.ru>
2022-12-08 00:57:58 +0300

    feat: add all user routes to api gateway

--- api-gateway-service/internal/users/routes.go
+++ api-gateway-service/internal/users/routes.go
@@ -21,7 +21,23 @@ func InitializeUsersRoutes(a fiber.Router, URL string, logger logger.Logger, aut
 	getUsersGroup.Get("/:id", svc.GetUser)
 	getUsersGroup.Get("/prefix", svc.GetUsersByNicknamePrefix)
 	getUsersGroup.Get("/nickname", svc.GetUserByNickname)
-	getUsersGroup.Group("/create", m.AuthRequired).Post("/", svc.CreateUser) // need authorization for user creation
+	getUsersGroup.Get("/friends", svc.GetUserByNickname)
+
+	protectedUsersGroup := getUsersGroup.Group("/protected", m.AuthRequired)
+	protectedUsersGroup.Post("/", svc.CreateUser)
+	protectedUsersGroup.Patch("/", svc.UpdateUserInfo)
+
+	userLinkGroup := protectedUsersGroup.Group("/links")
+	userLinkGroup.Post("/", svc.AddUserLink)
+	userLinkGroup.Get("/", svc.GetUserLinks)
+	userLinkGroup.Delete("/", svc.DeleteUserLink)
+
+	userFriendsGroup := protectedUsersGroup.Group("/friends")
+	userFriendsGroup.Get("/sent", svc.GetUserSentFriends)
+	userFriendsGroup.Get("/received", svc.GetUserReceivedFriends)
+	userFriendsGroup.Post("/", svc.SendFriend)
+	userFriendsGroup.Post("/accept", svc.AcceptFriend)
+	userFriendsGroup.Post("/decline", svc.DeclineFriend)
 
 	return svc
 }
@@ -44,5 +60,40 @@ func (svc ServiceClient) GetUserByNickname(ctx *fiber.Ctx) error {
 
 func (svc ServiceClient) CreateUser(ctx *fiber.Ctx) error {
 	return handlers.CreateUser(ctx, svc.Client)
+}
 
+func (svc ServiceClient) UpdateUserInfo(ctx *fiber.Ctx) error {
+	return handlers.UpdateUserInfo(ctx, svc.Client)
+}
+
+func (svc ServiceClient) AddUserLink(ctx *fiber.Ctx) error {
+	return handlers.AddUserLink(ctx, svc.Client)
+}
+
+func (svc ServiceClient) GetUserLinks(ctx *fiber.Ctx) error {
+	return handlers.GetUserLinks(ctx, svc.Client)
+}
+
+func (svc ServiceClient) DeleteUserLink(ctx *fiber.Ctx) error {
+	return handlers.DeleteUserLink(ctx, svc.Client)
+}
+
+func (svc ServiceClient) GetUserSentFriends(ctx *fiber.Ctx) error {
+	return handlers.GetUserSentFriends(ctx, svc.Client)
+}
+
+func (svc ServiceClient) GetUserReceivedFriends(ctx *fiber.Ctx) error {
+	return handlers.GetUserReceivedFriends(ctx, svc.Client)
+}
+
+func (svc ServiceClient) SendFriend(ctx *fiber.Ctx) error {
+	return handlers.SendFriend(ctx, svc.Client)
+}
+
+func (svc ServiceClient) AcceptFriend(ctx *fiber.Ctx) error {
+	return handlers.AcceptFriend(ctx, svc.Client)
+}
+
+func (svc ServiceClient) DeclineFriend(ctx *fiber.Ctx) error {
+	return handlers.DeclineFriend(ctx, svc.Client)
 }
